a31d156cb420334c622cadec948c31a8a140a5d0
krixon <ja457725@gmail.com>
2025-12-10 23:29:50 +0530

    menu and main file edited

--- src/main.c
+++ src/main.c
@@ -1,35 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "menu.h"
-#include "csv.h"
-#include "array_ops.h"
 
-int main() {
-    Image* images = NULL;
-    int count = 0;
-    int choice;
+int main()
+{
+    printf("====================================\n");
+    printf("   IMAGE METADATA MANAGEMENT SYSTEM\n");
+    printf("====================================\n");
 
-    while (1) {
-        showMenu();
-        scanf("%d", &choice);
-
-        if (choice == 1) {
-            images = loadCSV("data/images.csv", &count);
-            if (images != NULL)
-                printf("\n%d records loaded successfully!\n", count);
-
-        } else if (choice == 2) {
-            displayImages(images, count);
-
-        } else if (choice == 3) {
-            printf("Exiting...\n");
-            freeArray(images);
-            break;
-
-        } else {
-            printf("Invalid option!\n");
-        }
-    }
+    showMenu();   // NEW LINE
 
     return 0;
 }
--- src/menu.c
+++ src/menu.c
@@ -1,12 +1,49 @@
 #include <stdio.h>
 #include "menu.h"
 
-void showMenu() {
-    printf("\n==============================\n");
-    printf(" IMAGE METADATA MANAGEMENT\n");
-    printf("==============================\n");
-    printf("1. Load CSV\n");
-    printf("2. Display All Records\n");
-    printf("3. Exit\n");
-    printf("Choose an option: ");
+void showMenu()
+{
+    int choice;
+
+    while (1)
+    {
+        printf("\n=============================\n");
+        printf("        MAIN MENU\n");
+        printf("=============================\n");
+        printf("1. Load images from CSV\n");
+        printf("2. Display all images\n");
+        printf("3. Add new image\n");
+        printf("4. Delete an image\n");
+        printf("5. Save and Exit\n");
+        printf("=============================\n");
+        printf("Enter your choice: ");
+
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+            case 1:
+                printf("Loading CSV... (will implement later)\n");
+                break;
+
+            case 2:
+                printf("Displaying images... (will implement later)\n");
+                break;
+
+            case 3:
+                printf("Adding new image... (will implement later)\n");
+                break;
+
+            case 4:
+                printf("Deleting image... (will implement later)\n");
+                break;
+
+            case 5:
+                printf("Saving and exiting...\n");
+                return;   // Exit menu
+
+            default:
+                printf("Invalid choice! Try again.\n");
+        }
+    }
 }
